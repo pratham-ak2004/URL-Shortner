ad8c70aeac4e469acc1222618276b56d65555e83
pratham-ak2004 <pratham20442@gmail.com>
2023-11-30 14:19:34 +0530

    updated RedirectingController.java

--- src/main/java/com/urlshotener/url/shortner/controller/RedirectingController.java
+++ src/main/java/com/urlshotener/url/shortner/controller/RedirectingController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/redirect")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","http://localhost:4173"})
 public class RedirectingController {
 
     @Autowired
